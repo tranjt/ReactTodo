4f07295b9e0a7d3e7c61739ae0a1a6a370a9cf2b
JTTran <tranjt80@gmail.com>
2017-06-02 00:59:59 +0300

    Add static todo component

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var TodoApp = require("TodoApp");
 
 
 
@@ -12,7 +13,7 @@ $(document).foundation();
 require("style!css!applicationStyle");
 
 ReactDOM.render(    
-    <p>Boilerplate 3</p>,
+    <TodoApp/>,
     document.getElementById("app")
 );
 

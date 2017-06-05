68437a4bd171d36e4ebc3e05064c58d3cc78c743
JTTran <tranjt80@gmail.com>
2017-06-05 15:56:32 +0300

    Run webpack on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require("webpack");
 
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 
 module.exports = {
     entry: [
@@ -14,6 +16,11 @@ module.exports = {
         new webpack.ProvidePlugin({
             "$": "jquery",
             "jQuery": "jquery"
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
         })
     ],
     output: {
@@ -49,5 +56,5 @@ module.exports = {
         ]
     },
 
-    devtool: "cheap-module-eval-source-map"
+    devtool: process.env.NODE_ENV === "production" ? undefined : "cheap-module-eval-source-map"
 };
